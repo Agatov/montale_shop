c958619a61625a663cf0aa602ee16878b4e2ea59
agatov <agatovs@gmail.com>
2013-12-17 17:31:29 +0400

    fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ActiveRecord::Base
   belongs_to :family
   belongs_to :brand
   after_create :create_index
+  after_update :create_index
 
   as_enum :sex, [:unisex, :male, :female], prefix: true
   mount_uploader :image, ItemImageUploader
